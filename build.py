867ff139589d1fee7e6f1853229f65102d63c593
rayzchen <tankimarshal2@gmail.com>
2020-12-30 17:58:30 +0000

    Added build script

---
+++ build.py
@@ -0,0 +1,17 @@
+import os, shutil
+
+exclude = [
+    "asyncio", "concurrent", "ctypes", "disutils", "email", "html", "http", "lib2to3", "logging",
+    "multiprocessing", "pydoc_data", "test", "tkinter", "unittest", "urllib", "xml", "xmlrpc"]
+
+if os.path.isdir("dist"):
+    shutil.rmtree("dist")
+os.system("cxfreeze -c -OO main.py --include-files data --target-dir dist --excludes " + ",".join(exclude))
+
+os.chdir("dist/lib")
+for file in os.listdir():
+    if file not in ["collections", "encodings", "importlib", "library.zip"]:
+        if os.path.isfile(file):
+            os.remove(file)
+        else:
+            shutil.rmtree(file)
